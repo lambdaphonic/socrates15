4e3bf10cb8ccd616e8686f0ec8af3b941ddd6556
Martin Gondermann <magicmonty@pagansoft.de>
2015-08-27 23:31:32 +0200

    Fixed namespace

--- src/lambdaphonic/socrates15.clj
+++ src/lambdaphonic/socrates15.clj
@@ -1,7 +1,9 @@
-(ns lambdaphonic.wmfra
+(ns lambdaphonic.socrates15
   (use [overtone.live :refer :all]
        [mud.core]
        [lambdaphonic.overtone.helpers]
        [lambdaphonic.overtone.synths]
        [overtone.inst.drum])
   (require [shadertone.tone :as t]))
+
+
