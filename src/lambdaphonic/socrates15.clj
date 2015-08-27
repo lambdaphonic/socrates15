563b976463a345602319220a72e7773e7cf55ff2
Martin Gondermann <magicmonty@pagansoft.de>
2015-08-27 23:25:30 +0200

    Initial import

---
+++ src/lambdaphonic/socrates15.clj
@@ -0,0 +1,7 @@
+(ns lambdaphonic.wmfra
+  (use [overtone.live :refer :all]
+       [mud.core]
+       [lambdaphonic.overtone.helpers]
+       [lambdaphonic.overtone.synths]
+       [overtone.inst.drum])
+  (require [shadertone.tone :as t]))
